6075a8dc66b2d913243f4213c71257c94791da4e
Yurui Zhang <yuruiology@gmail.com>
2022-06-22 22:12:40 -0400

    adds basic logic

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,10 @@
 class GreetingsController < ApplicationController
   def hello
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "test", template: 'greetings/hello', formats: [:html]
+      end
+    end
   end
 end
